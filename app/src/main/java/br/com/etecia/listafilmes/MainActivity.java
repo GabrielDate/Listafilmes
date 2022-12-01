f0af3c61cd840eb2425c9ac3b9229d0dd5b163bd
Laboratorio-Info <g_gabrielvictor@hotmail.com>
2022-12-01 19:55:34 -0300

    criando projeto 0.1

--- app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
@@ -2,6 +2,9 @@ package br.com.etecia.listafilmes;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,9 +16,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listafilmes = findViewById(R.id.idlistafilmes);
+
+        MyAdapter adapter = new  MyAdapter();
+
         //preparar para instanciar o adaptador
-        listafilmes.setAdapter(Adaptador);
 
+        listafilmes.setAdapter(adapter);
 
+    }
+    //criando uma inner class MyAdapter
+    public class  MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
